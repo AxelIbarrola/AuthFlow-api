dc592566dbc287ef6715d4a1711c40479f02d5e1
Axel Ibarrola <axelibarro@gmail.com>
2025-04-08 12:43:04 -0300

    rotación de refresh tokens agregada

--- src/controllers/refresh_token.js
+++ src/controllers/refresh_token.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken')
 require('dotenv').config()
-const { generateToken } = require('../utils/generate_token')
+const { generateToken, generateRefreshToken } = require('../utils/generate_token')
 const { RefreshTokens } = require('../models/RefreshTokens')
 
 const refresh = async (req, res, next) => {
@@ -16,21 +16,34 @@ const refresh = async (req, res, next) => {
         const storedToken = await RefreshTokens.findOne({ where: {token: refreshToken }})
 
         if (!storedToken) {
-            res.status(403).json({error: '❌ Refresh token not found or revoked.'})
+            return res.status(403).json({error: '❌ Refresh token not found or revoked.'})
         }
         
-        jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, decoded) => {
+        const decoded = jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET)
 
-            if (err) {
-                return res.status(403).json({ error: '❌ Invalid refresh token.'})
-            }
-    
-            const accessToken = generateToken(decoded.user)
-    
-            res.status(200).json({ message: accessToken })
+        await storedToken.destroy()
+
+        const newAccessToken = generateToken({
+            id: decoded.user.id,
+            email: decoded.user.email })
+        
+        const newRefreshToken = generateRefreshToken({
+            id: decoded.user.id,
+            email: decoded.user.email
         })
 
-    } catch (error) {
+        await RefreshTokens.create({
+            token: newRefreshToken,
+            userId: decoded.user.id
+        })
+
+    
+        res.status(200).json({ 
+            accessToken: newAccessToken,
+            refreshToken: newRefreshToken
+        })
+
+        } catch (error) {
         next(error)
     }
 }
